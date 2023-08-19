299e471fe0c9a644c00e95536103dc2f9c808db3
Rahul <rahubisht0968@gmail.com>
2023-08-19 15:00:36 +0530

    Error page with responsiveness is being created

--- src/components/ErrorPage/ErrorPage.jsx
+++ src/components/ErrorPage/ErrorPage.jsx
@@ -1,9 +1,38 @@
 import React from 'react'
+import "./ErrorPage.scss"
+import { useNavigate } from "react-router-dom"
+import Pic from "../../assets/error_dog_png-removebg-preview.png"
 
 export default function ErrorPage() {
+
+    const navigate = useNavigate();
+
     return (
-        <div>
-            Error page
+        <div className='errorPageSection'>
+            <div className="errorPageContent">
+                <div className="errorContainer">
+                    <div className="errorStatus">
+                        404
+                    </div>
+                    <div className="errorImage">
+                        <img src={Pic} alt="" />
+                    </div>
+                    <div className="errorMessage">
+                        <span className="bigText">
+                            UH OH! You're lost.
+                        </span>
+                        <span className="smallText">
+                            The page you are looking for does not exist. How you got here is a mystery. But you can click the below button to go back to the Homepage.
+                        </span>
+                    </div>
+                    <div className="errorButton" onClick={() => {
+                        navigate('/')
+                        window.scrollTo({ top: 0, behavior: 'smooth' })
+                    }}>
+                        HOME
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
